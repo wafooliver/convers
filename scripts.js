870f49d5d770024a9a511f9a6cab43d0c3ed0f30
wafooliver <wafoolivierdylan@gmail.com>
2024-07-21 19:56:04 +0200

    Initial commit with currency converter application

--- scripts.js
+++ scripts.js
@@ -4,16 +4,19 @@ const exchangeRates = {
     GBP: { USD: 1.34, EUR: 1.14 }
 };
 
+
 function convertCurrency() {
     const amount = parseFloat(document.getElementById('amount').value);
     const fromCurrency = document.getElementById('fromCurrency').value;
     const toCurrency = document.getElementById('toCurrency').value;
 
+    // condiction de test si les la valeur entrer est negatif
     if (isNaN(amount) || amount <= 0) {
         document.getElementById('result').innerText = 'S\'il vous plait entrer un montant valide';
         return;
     }
 
+    // condiction pour se rassurer qu'on choisi deux devise differente
     if (fromCurrency === toCurrency) {
         document.getElementById('result').innerText = 'Veuillez sélectionner une devise différente';
         return;
